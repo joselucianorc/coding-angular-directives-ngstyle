b86420c1017176545333f86d8af412f6d6702dd2
José Luciano Julietti Rodrigues de Castro <joselucianorc@gmail.com>
2021-03-29 12:15:36 -0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgstyleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
